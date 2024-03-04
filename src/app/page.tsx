b8a3b35813f1f82c4aada04dc901ef7b7b54a2ee
Udaky <rizkyfakhri227@gmail.com>
2024-03-04 12:33:56 +0700

    file upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,41 @@
+import FileUpload from "@/components/FileUpload";
 import { Button } from "@/components/ui/button";
 import { UserButton, auth } from "@clerk/nextjs";
+import { LogIn } from "lucide-react";
+import Link from "next/link";
 
 export default async function Home() {
   const { userId } = await auth();
   const isAuth = !!userId;
   return (
-    <div className="b min-h-screen w-screen bg-gradient-to-r from-rose-100 to-teal-100">
+    <div className="min-h-screen w-screen bg-gradient-to-r from-rose-100 to-teal-100">
       <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <div className="flex flex-col items-center text-center">
           <div className="flex items-center">
             <h1 className="mr-3 text-5xl font-semibold">Chat with any PDF</h1>
             <UserButton afterSignOutUrl="/" />
-            <div className="mt-2 flex">
-              <Button>Go to Chats</Button>
-            </div>
+          </div>
+
+          <div className="mt-2 flex">
+            {isAuth && <Button>Go to Chats</Button>}
+          </div>
+
+          <p className="mt-1 max-w-xl text-lg text-slate-600">
+            Join million of students, researchers and profesionals to instantly
+            answer owner questions and understand research with AI
+          </p>
+
+          <div className="mt-4 w-full">
+            {isAuth ? (
+              <FileUpload />
+            ) : (
+              <Link href="/signin">
+                <Button>
+                  Login to get started
+                  <LogIn className="ml-2 h-4 w-4" />
+                </Button>
+              </Link>
+            )}
           </div>
         </div>
       </div>
---
+++ src/components/FileUpload.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { Inbox } from "lucide-react";
+import React from "react";
+import { useDropzone } from "react-dropzone";
+
+const FileUpload = () => {
+  const { getRootProps, getInputProps } = useDropzone({
+    accept: { "application/pdf": [".pdf"] },
+    maxFiles: 1,
+    onDrop: (acceptedFiles) => {
+      console.log(acceptedFiles);
+    },
+  });
+  return (
+    <div className="rounded-xl bg-white p-2">
+      <div
+        {...getRootProps({
+          className:
+            "border-dashed border-2 rounded-xl cursor-pointer bg-gray-50 py-8 flex justify-center items-center flex-col",
+        })}
+      >
+        <input {...getInputProps()} />
+        <>
+          <Inbox className="h-10 w-10 text-blue-500" />
+          <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
+        </>
+      </div>
+    </div>
+  );
+};
+
+export default FileUpload;
